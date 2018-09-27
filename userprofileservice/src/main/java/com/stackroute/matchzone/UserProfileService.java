fc02d3284cd00930098ebf3f4b6531a5183e9700
shalini811 <41104248+shalini811@users.noreply.github.com>
2018-09-27 11:25:21 +0530

    Update UserProfileService.java

--- userprofileservice/src/main/java/com/stackroute/matchzone/UserProfileService.java
+++ userprofileservice/src/main/java/com/stackroute/matchzone/UserProfileService.java
@@ -2,9 +2,10 @@ package com.stackroute.matchzone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+@ComponentScan(basePackages="com.stackroute.*")
 @SpringBootApplication
 @EnableEurekaClient // for eureka server to find this application
 public class UserProfileService {
