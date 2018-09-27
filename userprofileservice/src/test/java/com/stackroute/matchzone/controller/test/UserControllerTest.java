6de5468bc1bf7d7c9a3fd2d6a9b69c78a8d2c356
shalini811 <41104248+shalini811@users.noreply.github.com>
2018-09-27 12:36:17 +0530

    Update UserControllerTest.java

--- userprofileservice/src/test/java/com/stackroute/matchzone/controller/test/UserControllerTest.java
+++ userprofileservice/src/test/java/com/stackroute/matchzone/controller/test/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.stackroute.matchzone.controller.UserController;
 import com.stackroute.matchzone.exceptions.UserAlreadyExistsException;
 import com.stackroute.matchzone.exceptions.UserNotFoundException;
 import com.stackroute.matchzone.model.User;
+import com.stackroute.matchzone.repository.UserRepository;
 import com.stackroute.matchzone.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,25 +16,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.mockito.Mockito.when;
-
 @RunWith(SpringRunner.class)
 @WebMvcTest
+@WebAppConfiguration
 public class UserControllerTest {
-
-	
-
+    
     @Autowired
     private MockMvc mockMvc;
     @MockBean
@@ -42,7 +38,7 @@ public class UserControllerTest {
     private UserService userService;
     @InjectMocks
     private UserController userController;
-
+   
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -52,44 +48,27 @@ public class UserControllerTest {
         user.setUserPassword("123456");
         user.setUserEmail("jhonsimon@gmail.com");
     }
-
         @Test
-        public void registerUserSuccess() throws UserAlreadyExistsException, UserNotFoundException {
-
-            when(userService.createUser(user)).thenReturn(true);
-            assertNotNull(userService.getUser("Jhon Simon"));
+        public void registerUserSuccess() throws UserAlreadyExistsException {
+            
             try {
-				mockMvc.perform(post("/api/v1/user")
-				        .contentType(MediaType.APPLICATION_JSON).content(asJsonString(user)))
-				        .andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
+            	when(userService.createUser(user)).thenReturn(true);
+                
+                mockMvc.perform(post("/api/v1/user")
+                        .contentType(MediaType.APPLICATION_JSON).content(asJsonString(user)))
+                        .andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
-
-        @Test
-        public void registerUserFailure() throws UserAlreadyExistsException, UserNotFoundException {
-
-            when(userService.createUser(any())).thenThrow(UserAlreadyExistsException.class);
-            assertNotNull(userService.getUser("Jhon Simon"));
-            try {
-				mockMvc.perform(post("/api/v1/user")
-				        .contentType(MediaType.APPLICATION_JSON).content(asJsonString(user)))
-				        .andExpect(status().isConflict()).andDo(MockMvcResultHandlers.print());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-        }
+       
         @Test
         public void updateUserSuccess() throws UserNotFoundException {
-            user.setUserPassword("23456789");
-            assertNotNull(userService.getUser("Jhon Simon"));
-            when(userService.updateUser(eq(user.getUserEmail()), any())).thenReturn(user);
+            
             try {
+            	user.setUserPassword("23456789");
+                when(userService.updateUser(eq(user.getUserEmail()), any())).thenReturn(user);
 				mockMvc.perform(put("/api/v1/user/Jhon123")
 				        .contentType(MediaType.APPLICATION_JSON).content(asJsonString(user)))
 				        .andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
@@ -97,28 +76,11 @@ public class UserControllerTest {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
         }
-
-        @Test
-        public void updateUserFailure() throws UserNotFoundException {
-                user.setUserPassword("23456789");
-                assertNotNull(userService.getUser("Jhon Simon"));
-                when(userService.updateUser(eq(user.getUserEmail()), any())).thenThrow(UserNotFoundException.class);
-                try {
-					mockMvc.perform(put("/api/v1/user/Jhon123")
-					        .contentType(MediaType.APPLICATION_JSON).content(asJsonString(user)))
-					        .andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-        }
+        
         @Test
         public void deleteUserSuccess() throws UserNotFoundException {
             when(userService.deleteUser("Jhon123")).thenReturn(true);
-            assertNotNull(userService.getUser("Jhon Simon"));
             try {
 				mockMvc.perform(delete("/api/v1/user/Jhon123")
 				        .contentType(MediaType.APPLICATION_JSON))
@@ -128,30 +90,11 @@ public class UserControllerTest {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
         }
-
-
-        @Test
-        public void deleteUserFailure() throws UserNotFoundException {
-            when(userService.deleteUser("Jhon123")).thenThrow(UserNotFoundException.class);
-            assertNotNull(userService.getUser("Jhon Simon"));
-            try {
-				mockMvc.perform(delete("/api/v1/user/Jhon123")
-				        .contentType(MediaType.APPLICATION_JSON))
-				        .andExpect(status().isNotFound())
-				        .andDo(MockMvcResultHandlers.print());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-        }
+        
         @Test
         public void getByUserIdSuccess() throws UserNotFoundException {
-
             when(userService.getUser("Jhon123")).thenReturn(user);
-            assertNotNull(userService.getUser("Jhon Simon"));
             try {
 				mockMvc.perform(get("/api/v1/user/Jhon123").contentType(MediaType.APPLICATION_JSON))
 				        .andExpect(status().isOk())
@@ -161,29 +104,15 @@ public class UserControllerTest {
 				e.printStackTrace();
 			}
         }
-
-        @Test
-        public void getByUserIdFAilure() throws UserNotFoundException {
-
-            when(userService.getUser("Jhon123")).thenThrow(UserNotFoundException.class);
-            assertNotNull(userService.getUser("Jhon Simon"));
-            try {
-				mockMvc.perform(get("/api/v1/user/Jhon123").contentType(MediaType.APPLICATION_JSON))
-				        .andExpect(status().isNotFound())
-				        .andDo(MockMvcResultHandlers.print());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-
-
+        
         public static String asJsonString(final Object obj) {
             try {
                 return new ObjectMapper().writeValueAsString(obj);
             } catch (Exception e) {
-            	return e.getMessage();
+                return e.getMessage();
             }
         }
-
 }
+
+
+
