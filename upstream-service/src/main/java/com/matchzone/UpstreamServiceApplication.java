49cc9da6d8705dd38534c748ce6cb7155a768cf1
Sashi Kumar Singh <shashi12singh@gmail.com>
2018-10-11 11:36:02 +0530

    Update UpstreamServiceApplication.java

--- upstream-service/src/main/java/com/matchzone/UpstreamServiceApplication.java
+++ upstream-service/src/main/java/com/matchzone/UpstreamServiceApplication.java
@@ -3,6 +3,7 @@ package com.matchzone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//Annotate this class with @EnableEurekaClient and @SpringBootApplication
 @EnableEurekaClient
 @SpringBootApplication
 public class UpstreamServiceApplication {
